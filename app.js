0c42b0f9935849fa3305bfe856db7551781f7b4e
AGrush <grushevskiy@gmail.com>
2021-01-11 11:07:40 +0000

    commit 2

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-const express = require('express')
-
+const express = require('express');
+const mongoose = require('mongoose');
+//use dotenv to store secret keys
+require('dotenv/config')
 const app = express();
+
+//middleware
+// app.use('/posts', () => {
+//   console.log('This is a middleware running');
+// })
+
+function requireAdmin(req, res, next) {
+  console.log('middleware tings')
+  next();
+}
+
 
 //ROUTES
 app.get('/', (req,res) => {
   res.send('we are on home')
 })
 
+app.get('/posts', requireAdmin, (req,res) => {
+  res.send('we are on posts')
+})
+
+//connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  console.log('connected to DB!')
+})
+
+
+
 //How do we start listening to the server
-app.listen(3000);
+app.listen(3001);
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Commit 1:
+
+Basic node server with home route, npm start then go localhost:3000;
+
+## Commit 2:
+
+Connect with database:
+Setup atlas database & connect with mongoose, hide the connection key insid dotenv using process.env.DB_CONNECTION instead of hardcoding the connection string.
