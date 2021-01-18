a9c7d1da69b96a4f653248f035161a9369ab3bd6
AGrush <grushevskiy@gmail.com>
2021-01-18 12:42:45 +0000

    sdfdfdfdg

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 //use dotenv to store secret keys
 require("dotenv").config();
-
-
-
-
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 
-mongoose.set('debug', true);
 
-//connect to DB
+
+
+//import routes for middleware
+const postsRoute = require('./routes/posts');
+
+//midleware routes
+app.use('/posts', postsRoute)
+
+//ROUTES
+app.get('/', (req,res) => {
+  res.send('we are on home')
+})
+
+
+//CONNECT TO DB
 mongoose.connect("mongodb+srv://grushevskiy:intercom@cluster-rest.4luv0.mongodb.net/cluster-rest?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, dbName: "cluster-rest" }, () => {
   console.log('connected to DB!')
 })
+
+mongoose.set('debug', true);
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+    console.log('connection opened')
+});
+
 
 //MIDDLEWARES
 //cors
@@ -23,33 +41,6 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 //parse json POSTs
 app.use(express.json());
-
-
-//middleware
-// app.use('/posts', () => {
-
-//   console.log('This is a middleware running');
-// })
-
-// function requireAdmin(req, res, next) {
-//   console.log('middleware tings')
-//   next();
-// }
-
-//import routes for middleware
-const postsRoute = require('./routes/posts');
-
-//midleware routes
-app.use('/posts', postsRoute)
-
-
-//ROUTES
-app.get('/', (req,res) => {
-  res.send('we are on home')
-})
-
-
-
 
 
 //How do we start listening to the server
