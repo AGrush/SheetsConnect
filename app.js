c512dae70b8bd3a84ba0e213b96fd450352581aa
AGrush <grushevskiy@gmail.com>
2021-01-18 16:45:47 +0000

    sdfdfdfdg

--- app.js
+++ app.js
@@ -2,20 +2,13 @@
 require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
 const cors = require('cors');
+const app = express();
 
-//MIDDLEWARES
-//cors
-app.use(cors());
-//decode url special characters
-app.use(express.urlencoded({ extended: true }));
-//parse json POSTs
-app.use(express.json());
 
 //connect to DB
-mongoose.connect("mongodb+srv://grushevskiy:intercom@cluster-rest.4luv0.mongodb.net/cluster-rest?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, dbName: "cluster-rest" }, () => {
-  console.log('connected to DB!')
+mongoose.connect("mongodb+srv://grushevskiy:intercom@cluster0.9mmia.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, dbName: "cluster-rest" }).then(() => {
+  console.log('MongoDB is connected')
 })
 
 mongoose.set('debug', true);
@@ -25,6 +18,19 @@ const db = mongoose.connection;
 db.once('open', () => {
     console.log('connection opened')
 });
+
+
+
+//MIDDLEWARES
+//cors
+app.use(cors());
+//decode url special characters
+app.use(express.urlencoded({ extended: true }));
+//parse json POSTs
+app.use(express.json());
+
+
+
 
 
 //import routes for middleware
