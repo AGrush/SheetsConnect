ad24358c575ad1dd0e680200e758f78ddbf9102a
AGrush <grushevskiy@gmail.com>
2021-01-28 19:01:04 +0000

    sdfdfdfdgfdkdd

--- routes/posts.js
+++ routes/posts.js
@@ -164,10 +164,7 @@ router.delete('/:postId', async (req,res) => {
 //DELETE LAST POST
 router.delete('/last', async (req,res) => {
   try {
-    const removedPost = await Post.findOneAndDelete(
-      { __v: 0 },
-      { sort: { "date": -1 } }
-    );
+    const removedPost = await Post.findOneAndDelete({},{"sort": { "_id": -1 }});
     res.json(removedPost)
   }catch(err){
     res.json({ message: err })
